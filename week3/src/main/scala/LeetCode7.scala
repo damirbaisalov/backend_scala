fd16eba61db41aed21f6f2971e981cd1a9fc4da2
damirbaisalov <baisalovdamir@gmail.com>
2021-02-18 19:16:03 +0600

    task 7

--- week3/src/main/scala/LeetCode7.scala
+++ week3/src/main/scala/LeetCode7.scala
@@ -1,7 +1,27 @@
 object LeetCode7 extends App {
 
-//  def kWeakestRows(mat: Array[Array[Int]], k: Int): Array[Int] = {
-//
-//  }
+  def kWeakestRows(mat: Array[Array[Int]], k: Int): Array[Int] = {
+    val y = mat.length
+    val x = mat(0).length
+    var kix = 0
+
+    var vis: Array[Int] = new Array[Int](y)
+    var result: Array[Int] = new Array[Int](k)
+
+    for (j <- 0 to x) {
+      for (i <- 0 until y) {
+        if (vis(i) == 0 && (j == x || mat(i)(j) == 0)) {
+          result(kix)= i
+          kix+=1
+          vis(i)+=1
+        }
+        if (kix == k) {
+          return result
+        }
+      }
+    }
+
+    result
+  }
 
 }

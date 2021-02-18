f293b7343f45cee352f683f7301b32f2a055f820
damirbaisalov <baisalovdamir@gmail.com>
2021-02-18 11:30:02 +0600

    task 9 and 10

--- week3/src/main/scala/LeetCode10.scala
+++ week3/src/main/scala/LeetCode10.scala
@@ -2,16 +2,25 @@ object LeetCode10 extends App {
   def buildArray(target: Array[Int], n: Int): List[String] = {
     var result: List[String] = List[String]()
     var start = 1
-    for (i <- target.indices) {
-      if (target(i) == start) {
-        result +:="push"
-      } else {
-        result +:="push"
-        result +:="pop"
+    for(i <- target) {
+      for(j <- start until i) {
+        result :+= "Push"
+        result :+= "Pop"
       }
-      start=start+1
+      result :+= "Push"
+      start = i + 1
     }
-
     result
   }
+
+  def testCase(): Unit = {
+    val target = Array(1,3)
+    val n = 3
+    var result = buildArray(target,n)
+
+    for (i <- result)
+      print(i + " ")
+  }
+
+  testCase()
 }
---
+++ week3/src/main/scala/LeetCode8.scala
@@ -0,0 +1,3 @@
+object LeetCode8 {
+
+}
